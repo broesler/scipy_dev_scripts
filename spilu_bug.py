20fa54e2b0729639c085db57f567884e921c97c5
Bernard Roesler <bernard.roesler@gmail.com>
2025-12-16 16:11:32 -0500

    Clean up spilu_bug.py before submitting bug report.
    See bug report:
    [SciPy Issue #24171](https://github.com/scipy/scipy/issues/24171).

--- spilu_bug.py
+++ spilu_bug.py
@@ -8,15 +8,16 @@
 # =============================================================================
 
 import numpy as np
-from numpy.testing import assert_array_equal, assert_allclose
+from numpy.testing import assert_allclose as _assert_allclose
+from numpy.testing import assert_array_equal
 from scipy import linalg as la
 from scipy.sparse import csc_array
 from scipy.sparse import linalg as spla
 
 
-def allclose(a, b, atol=1e-15):
+def assert_allclose(a, b, atol=1e-15):
     """Check if two arrays are close to each other."""
-    return assert_allclose(a, b, atol=atol)
+    return _assert_allclose(a, b, atol=atol)
 
 
 # See: Davis, Timothy A. (2006). Direct Methods for Sparse Linear Systems,
@@ -31,18 +32,14 @@ cols = np.array(
     dtype=np.int32,
 )
 
-rng = np.random.default_rng(565656)
-vals = rng.random(len(rows), dtype=np.float64)
+# rng = np.random.default_rng(565656)
+# vals = rng.random(len(rows), dtype=np.float64)
+vals = np.ones(len(rows), dtype=np.float64)
 
-# vals = np.ones(len(rows), dtype=np.float64)
-vals[:7] = np.arange(1, 8, dtype=np.float64)  # make diagonal entries non-unit
+# make diagonal entries non-unit to avoid pivoting
+vals[:7] = np.arange(1, 8, dtype=np.float64)
+
 A = csc_array((vals, (rows, cols)), shape=(N, N))
-
-# TODO permute rows
-# # ---------- Permute the matrix rows arbitrarily
-# p_input = np.r_[5, 1, 7, 0, 2, 6, 4, 3]
-# # p_input_inv = np.argsort(p_input)  # [3, 1, 4, 7, 6, 0, 5, 2]
-# A = A[p_input, :]
 
 # -----------------------------------------------------------------------------
 #         Test Incomplete LU decomposition
@@ -51,45 +48,50 @@ A = csc_array((vals, (rows, cols)), shape=(N, N))
 Ad = A.toarray()
 pd, Ld, Ud = la.lu(Ad, p_indices=True)
 
-allclose(Ld[pd] @ Ud, Ad)
+assert_allclose(Ld[pd] @ Ud, Ad)
+
+# Get (complete) sparse LU for comparison
+lu = spla.splu(A, permc_spec="NATURAL")
+p, L, U = lu.perm_r, lu.L, lu.U
+
+assert_array_equal(p, np.arange(N))  # no column permutation
+
+print("A:")
+print(Ad)
+print("L:")
+print(L.toarray())
+print("U:")
+print(U.toarray())
 
 # NOTE spilu does not drop L entries that are < drop_tol?
 #  * In SuperLU, 0 <= tol <= 1, because the drop tolerance is a fraction of the
 #    maximum entry in each column.
-
-# drop_tol = 0.0  # keep everything
-# drop_tol = 0.08
-drop_tol = 1.0  # drop everything off-diagonal -> FIXME does nothing?
+# drop_tol = 0.0     # keep everything
+# drop_tol = 0.08    # arbitrary, should drop 1 entry L[3, 6]
+# drop_tol = 1.0     # drop everything off-diagonal -> FIXME does nothing?
 # drop_tol = np.inf  # drop everything off-diagonal -> FIXME does nothing?
 
-lu = spla.splu(A, permc_spec="NATURAL")
-ilu = spla.spilu(A, drop_tol=drop_tol, permc_spec="NATURAL")
+for drop_tol in [0.0, 1.0, np.inf]:
+    # Compute the incomplete LU decomposition
+    ilu = spla.spilu(A, drop_tol=drop_tol, permc_spec="NATURAL")
+    pi, Li, Ui = ilu.perm_r, ilu.L, ilu.U
 
-p, L, U = lu.perm_r, lu.L, lu.U
-pi, Li, Ui = ilu.perm_r, ilu.L, ilu.U
+    assert_array_equal(p, pi)
 
-assert_array_equal(p, np.arange(N))
-assert_array_equal(p, pi)
-
-print(f"---------- ilu ({drop_tol=}):")
-print("L:")
-print(L.toarray())
-print("Li:")
-print(Li.toarray())
-print("U:")
-print(U.toarray())
-print("Ui:")
-print(Ui.toarray())
-
-if drop_tol == 0:
-    allclose(Li.toarray(), L.toarray())
-    allclose(Ui.toarray(), U.toarray())
-    allclose(Li.toarray(), Ld)
-    allclose(Ui.toarray(), Ud)
-    allclose((Li[pi] @ Ui).toarray(), Ad)
-elif drop_tol >= 1:  # only diagonals
-    allclose(Li.toarray(), np.eye(Li.shape[0]))  # FIXME both fail!
-    allclose(Ui.diagonal(), A.diagonal())
+    if drop_tol == 0:
+        assert_allclose(Li.toarray(), L.toarray())
+        assert_allclose(Ui.toarray(), U.toarray())
+        assert_allclose(Li.toarray(), Ld)
+        assert_allclose(Ui.toarray(), Ud)
+        assert_allclose((Li[pi] @ Ui).toarray(), Ad)
+    elif drop_tol >= 1:  # only diagonals
+        print(f"---------- ilu ({drop_tol=})")
+        print("Li:")
+        print(Li.toarray())
+        print("Ui:")
+        print(Ui.toarray())
+        # assert_allclose(Li.toarray(), np.eye(Li.shape[0]))  # FIXME both fail!
+        # assert_allclose(Ui.diagonal(), A.diagonal())
 
 # =============================================================================
 # =============================================================================
